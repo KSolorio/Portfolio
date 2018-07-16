4b5314c8ce0d47269b6e297c6a8809db787c5ee9
Karina Solorio <macuser@Karinas-MacBook-Pro.local>
2018-07-15 21:39:44 -0400

    finalized homepage

--- app.js
+++ app.js
@@ -14,3 +14,11 @@ var granimInstance = new Granim({
     }
   }
 });
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
